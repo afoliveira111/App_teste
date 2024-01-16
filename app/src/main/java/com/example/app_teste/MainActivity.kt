2da20239696cb9c2dc72aa34a2a19030bc39bfa9
Felipe <felipetermometria@hotmail.com>
2024-01-16 18:24:05 -0300

    findView up

--- app/src/main/java/com/example/app_teste/MainActivity.kt
+++ app/src/main/java/com/example/app_teste/MainActivity.kt
@@ -6,12 +6,29 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var contador = 0
+    private var contador2 = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.botao)
+        val button2 = findViewById<Button>(R.id.botao2)
         val resultado = findViewById<TextView>(R.id.resultado)
-        resultado.text = "Testando"
+        val resultado2 = findViewById<TextView>(R.id.resultado2)
+
+        button.setOnClickListener {
+            contador = contador + 1
+            resultado.text = "Valor : ${contador}"
+
+         button2.setOnClickListener {
+             contador2 = contador2 -1
+             resultado2.text = "Valor : ${contador2}"
+         }
+
+        }
+
     }
 
 }
