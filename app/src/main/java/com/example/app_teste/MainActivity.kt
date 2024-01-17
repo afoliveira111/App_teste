89eb49a598e14358f883a31f2fb22144e1964a04
Felipe <felipetermometria@hotmail.com>
2024-01-17 18:08:08 -0300

    2 button and text

--- app/src/main/java/com/example/app_teste/MainActivity.kt
+++ app/src/main/java/com/example/app_teste/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private var contador = 0
-    private var contador2 = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +15,14 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.botao)
         val button2 = findViewById<Button>(R.id.botao2)
         val resultado = findViewById<TextView>(R.id.resultado)
-        val resultado2 = findViewById<TextView>(R.id.resultado2)
 
         button.setOnClickListener {
             contador = contador + 1
             resultado.text = "Valor : ${contador}"
 
          button2.setOnClickListener {
-             contador2 = contador2 -1
-             resultado2.text = "Valor : ${contador2}"
+             contador = contador -1
+             resultado.text = "Valor : ${contador}"
          }
 
         }
